22d8f6b11b62988ddb98aed14a30f79f8c70e339
windrim <martin.allen@theijf.org>
2023-11-11 22:53:57 -0500

    some text and colouring too, on the grid

--- game.py
+++ game.py
@@ -1,22 +1,29 @@
 import sys
+import time
 from random import randint
 
 import pygame
+
+# colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 WIDTH = 2000
 HEIGHT = 2000
 
 
-def square_size(n: int, m: int, width: int, height: int) -> tuple[int, int]:
+def tile_size(n: int, m: int, width: int, height: int) -> tuple[int, int]:
     """Given NxM dimensions of grid, and the display width and height,
     returns the WxL dimensions of one square in the grid."""
 
     return width // n, height // m
 
 
-def draw_grid(
+def disco_grid(
     screen, n: int, m: int, xsize: int, ysize: int, width: int, height: int
 ) -> None:
+    """This function shows an example of drawing a grid with randomly coloured squares.
+    It uses all the spatial parameters we use in the real setup."""
     for x in range(n, width, xsize):
         for y in range(m, height, ysize):
             rect = pygame.Rect(x, y, xsize, ysize)
@@ -25,14 +32,38 @@ def draw_grid(
             )
 
 
+def draw_grid(
+    screen, n: int, m: int, xsize: int, ysize: int, width: int, height: int
+) -> None:
+    """This function shows an example of drawing a grid with randomly coloured squares.
+    It uses all the spatial parameters we use in the real setup."""
+
+    font = pygame.font.Font(None, 10)
+    for x in range(n, width, xsize):
+        for y in range(m, height, ysize):
+            drawn = randint(0, 1)
+            if drawn:
+                rect = pygame.Rect(x, y, xsize, ysize)
+                pygame.draw.rect(
+                    screen, (randint(0, 255), randint(0, 255), randint(0, 255)), rect
+                )
+            else:
+                text = font.render("X", True, WHITE, BLACK)
+                text_rect = text.get_rect()
+                text_rect.center = x + xsize, y + ysize
+                screen.blit(text, text_rect)
+
+
 def main(n: int, m: int):
     """Main loop of game, taking board dimensions as arguments."""
     pygame.init()
 
+    pygame.display.set_caption("Caracassonne")
     screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
     screen.fill((0, 0, 0))
 
-    size = square_size(n, m, WIDTH, HEIGHT)
+    xsize, ysize = tile_size(n, m, WIDTH, HEIGHT)
 
     running = True
     while running:
@@ -41,7 +72,8 @@ def main(n: int, m: int):
                 running = False
                 continue
 
-        draw_grid(screen, n, m, size[0], size[1], WIDTH, HEIGHT)
+        # disco_grid(screen, n, m, size[0], size[1], WIDTH, HEIGHT)
+        draw_grid(screen, n, m, xsize, ysize, WIDTH, HEIGHT)
 
         pygame.display.flip()
 
@@ -51,7 +83,6 @@ def main(n: int, m: int):
 
 
 if __name__ == "__main__":
-    # TODO: cli
-    N = 20
-    M = 20
+    N = 100
+    M = 100
     main(N, M)
