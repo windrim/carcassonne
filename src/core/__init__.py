5edd6048fec982eb2ec9a46a0816394dc251687c
windrim <martin.allen@theijf.org>
2023-11-13 20:57:12 -0500

    wip: constraint now understood but refactoring required

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,13 +1,12 @@
 import csv
 import sys
-from dataclasses import dataclass
 from pathlib import Path
 from queue import LifoQueue
 from random import choice
 from typing import Any, Literal
 
 import numpy as np
-from square import Direction, Square, choose_value
+from square import Square, choose_value, constrain, neighbour_coords
 
 # number of possible tiles
 T = 113
@@ -36,7 +35,7 @@ def load_tiles() -> tuple[Any, Any]:
 def create_grid(n: int, m: int, t: int) -> Any:
     """Initializes the grid as NxM, each element being an empty Square "struct"."""
     return np.array(
-        [[Square(value=-1, rang=np.ones(t)) for _ in range(m)] for _ in range(n)]
+        [[Square(value=-1, domain=np.ones(t)) for _ in range(m)] for _ in range(n)]
     )
 
 
@@ -65,7 +64,7 @@ def collapse(i: int, j: int, grid: Any, entropies: Any) -> None:
     square.value = choose_value(square)
 
 
-def wave_function_collapse(grid: Any, entropies: Any) -> Literal[0, 1]:
+def wave_function_collapse(grid: Any, entropies: Any, tileset: Any) -> Literal[0, 1]:
     """Main algorithm. Eventually runs once per all tiles in grid. Returns 1 when grid completed,
     else 0.
 
@@ -95,9 +94,16 @@ def wave_function_collapse(grid: Any, entropies: Any) -> Literal[0, 1]:
     while not stack.empty():
         square = stack.get()
 
-        for direction in Direction:
-            ni, nj = ...
-            breakpoint()
+        for direction in range(4):
+            # each square can have no neighbour in a direction if it is on that edge;
+            # in this case, we get (-1, -1) from this function
+            ncords = neighbour_coords(k, l, len(grid), len(grid[0]), direction)
+            if ncords[0] != -1:
+                ni, nj = ncords
+                # check if this neighbour is already collapsed
+                if entropies[ni][nj] < T + 1:
+                    constrained = constrain(grid, entropies, tileset, ni, nj, direction)
+                    breakpoint()
 
     return 0
 
@@ -105,7 +111,7 @@ def wave_function_collapse(grid: Any, entropies: Any) -> Literal[0, 1]:
 def main(n: int, m: int) -> None:
     """Full runtime of the board generator."""
     # loading data from file
-    # files, meta = load_tiles()
+    files, meta = load_tiles()
 
     # initializing grids
     grid = create_grid(n, m, T)
@@ -113,7 +119,7 @@ def main(n: int, m: int) -> None:
 
     # collapsing tiles until seeing exit signal
     while True:
-        res = wave_function_collapse(grid, entropies)
+        res = wave_function_collapse(grid, entropies, meta)
         if res == 1:
             break
 
--- src/core/square.py
+++ src/core/square.py
@@ -1,59 +1,105 @@
 import random
 from dataclasses import dataclass
-from enum import Enum, auto
-from typing import Any
+from enum import IntEnum
+from typing import Any, Literal
 
 import numpy as np
 
-
-class Direction(Enum):
-    """Model of directions in which some square may have a neighbour."""
-
-    NORTH = auto()
-    EAST = auto()
-    SOUTH = auto()
-    WEST = auto()
-
+# Enums didn't work out here lmao
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
+Direction = Literal[0, 1, 2, 3, 4]
 
 @dataclass(slots=True)
 class Square:
     """Representation of one square on the grid.
     - tile index; -1 if empty
-    - range of possible values in this position, as bit array of length t
+    - domain of possible values in this position, as bit array of length t
 
     Note that Squares have entropy, but these are stored in a separate array
     for faster searching/updating."""
 
     value: int
-    rang: Any
+    domain: Any
 
 
 def choose_value(square: Square) -> int:
     """Given Square, pick a random legal choice of value."""
     # TODO: add weights etc
-    indices_of_possible = np.argwhere(square.rang == 1)
+    indices_of_possible = np.argwhere(square.domain == 1)
     return random.choice(indices_of_possible)
 
 
-def neighbour_coords(i: int, j: int, n: int, m: int, direction: Direction) -> tuple[int, int] | None:
-    """Returns co-ordinates of neighbour, given direction. Returns None if
+def neighbour_coords(i: int, j: int, n: int, m: int, direction: int) -> tuple[int, int]:
+    """Returns co-ordinates of neighbour, given direction. Returns -1, -1 if
     square @ [i, j] has no neighbour in that direction."""
-    # TODO: this isn't done yet
+    k, l = -1, -1
     match direction:
-        case Direction.NORTH:
-            if j == 0:
-                return None
-            return i, j + 1
-        case Direction.EAST:
-            if i == n:
-                return None
-            return i + 1, j
-        case Direction.SOUTH:
-            if j == m:
-                return None
-            return i + 1, j
-        case Direction.WEST:
-            if j == 0:
-                return None
-            return i + 1, j
+        case 0:
+            if j != 0:
+                k, l = i, j - 1
+        case 1:
+            if i != n:
+                k, l = i + 1, j
+        case 2:
+            if j != m:
+                k, l = i, j + 1
+        case 3:
+            if j != 0:
+                k, l = i - 1, j
+    return k, l
+
+def constrain(
+    grid: Any,
+    entropies: Any,
+    tileset: Any,
+    i: int,
+    j: int,
+    direction: int
+) -> bool:
+    """Checks and maybe constrains possible values for given Square at [i, j], which 
+    is adjacent to some collapsed or constrained Square, in the provided direction. 
+
+    # Propagating Constraints
+    
+    E.g., we collapse/constrain a square and check if its north neighbour should also
+    be constrained. We then give the co-ordinates corresponding to that neighbour, and
+    the north direction to this function.
+    
+    Returns True if square was constrained, signalling we must check all *this* square's
+    neighbours for constraint too. This creates a "wave" (??) of constrain() calls that 
+    propagates over the original, collapsed square's nearby neighbours. 
+
+    # Constraining
+
+    When this is called, we know either that the antecedent square has changed its domain.
+    This can mean it was collapsed, i.e. domain_n = 1, or just constrained to a lower amount.
+
+    In this case, we check if this square's domain must also be constrained. For example,
+    if the antecedent tile now shares only road or city edges with this square, we can't
+    have tiles with a grass edge on that side.
+
+    """
+
+    # TODO: refactor to a standard co-ord type
+    # TODO: pass co-ord pairs for both antecedent and current to this
+    # TODO: do some linalg to update the range of current, using range
+    # of ante, with tileset somehow in between ...
+
+    constrained = False
+
+    # Square we will constrain, a neighbour (of 1 or more steps away) of the
+    # one we just collapsed
+    square = grid[i][j]
+
+    # Direction of antecedent square
+    ante_dir = (direction + 2) % 4
+    ante_cords = neighbour_coords(i, j, len(grid), len(grid[0]), ante_dir)
+    ante = grid[i][j]
+
+    breakpoint()
+
+    return constrained
     
